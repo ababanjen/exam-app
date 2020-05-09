bd53cc61133b1e44f5cac477d3a0020a89cf2b22
Jenelyn Ababan <jenelynababan@priume.com>
2020-05-09 16:44:14 +0800

    slide per dot

--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -8,12 +8,12 @@ const ImageSlider = () => {
     const [state, setState] = useState(initialState)
     const {current} = state
 
-    const handleSlide = (isNext) => {
+    const handleSlide = ({isNext,isDot}) => {
         let val = isNext ? current+1 : current-1
         if(val > headerImages.length) val = 1
         if(val < 1) val = headerImages.length
         setState({
-            current: val
+            current: isDot ? isDot : val
         })
     }
     return(
@@ -35,10 +35,12 @@ const ImageSlider = () => {
             </section>
             <div className="slide-btn">
                 <div onClick={()=>handleSlide()} >&#10094;</div>
-                <div onClick={()=>handleSlide(true)} className="slide-btn right">&#10095;</div>
+                <div onClick={()=>handleSlide({isNext:true})} className="slide-btn right">&#10095;</div>
                 <div className="dot-wrapper">
                     {headerImages.map((s, i) => (
-                        <span className={i === current-1 ? 'dot active' : 'dot'}></span>
+                        <span className={i === current-1 ? 'dot active' : 'dot'}
+                        onClick={()=>handleSlide({isDot:i+1})}
+                        ></span>
                     ))}
                 </div>
             </div>
