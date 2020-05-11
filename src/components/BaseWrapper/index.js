fcecb60d930c24435c5c88bcadbba339f9b0b4e4
Jenelyn Ababan <jenelynababan@priume.com>
2020-05-11 11:40:16 +0800

    master

--- src/components/BaseWrapper/index.js
+++ src/components/BaseWrapper/index.js
@@ -3,7 +3,7 @@ import Header from './Header'
 import Footer from './Footer'
 const BaseWrapper = ({children}) => {
     const handleScrollToTop = () => {
-        window.scrollTo({top: 0});
+        window.scrollTo({top: 0,  behavior: 'smooth' });
     }
     return(
         <div id="page-container">
